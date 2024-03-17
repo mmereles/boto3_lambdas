b33112e08c09cda351cf6cb10485894c77786bf1
mmereles <60193314+mmereles@users.noreply.github.com>
2024-03-17 10:37:28 -0300

    Create put_order.py

---
+++ put_order.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    request_body = json.loads(event['body'])
+    order_id = request_body['order_id']
+    new_status = request_body['new_status']
+    customer_name = request_body['customer_name']
+    return {
+        'statusCode': 200,
+        'body': json.dumps(update_order_status_in_dynamodb(order_id, new_status, customer_name))
+    }
+
+def update_order_status_in_dynamodb(order_id, new_status, customer_name):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('CoffeOrders')
+
+    try:
+        table.update_item(
+            Key=[
+                'OrderId': order_id,
+                'CustomerName': customer_name
+            ],
+            UpdateExpression='SET OrderStatus = :status',
+            ExpressionAttributeValues={
+                ":status": new_status
+            }
+        )
+      return {'message': 'Order status updated successfully!', 'OrderId': order_id}
+    except Exception as e:
+        return {'error': str(e)}
