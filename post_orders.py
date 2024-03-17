2cc02d4bb59f712743f9f0b35e759a7d1b4df94d
mmereles <60193314+mmereles@users.noreply.github.com>
2024-03-17 10:21:25 -0300

    Create post_orders.py

---
+++ post_orders.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import uuid
+
+def lambda_handler(event, context):
+    request_body = json.loads(event['body'])
+    customer_name = request.body['customer_name']
+    coffee_blend = request_body['coffee_blend']
+    return {
+        'statusCode': 200,
+        'body': json.dumps(create_order_in_dynamodb(customer_name, coffee_blend))
+    }
+
+def create_order_in_dynamodb(customer_name, coffee_blend):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('CoffeeOrders')
+
+    order_id = str(uuid.uuid4())
+
+    try:
+      table.put_item(
+          Item={
+              'OrderId: order_id,
+              'CustomerName': customer_name,
+              'CoffeBlend': coffee_blend,
+              'Order_Status': Pending
+          }
+      )
+      return {'message': 'Order created successfully!', 'OrderId': order_id}
+    except Exception as e:
+        return {'error': str(e)}
